90f64d4668ffff92819671ca6c72809b052719a5
Shawanda Davis <shawandadavis93@gmail.com>
2018-12-27 17:35:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,8 +8,9 @@ class Shoe
   
   def cobble
     if @condition == "old"
-      puts "Your shoe is as good as new!" 
       @condition = "new"
+      puts "Your shoe is as good as new!" 
+
     end  
   end
   
