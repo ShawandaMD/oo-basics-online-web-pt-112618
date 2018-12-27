7cf4728b02aea3253211cb3dbc2d4e69eb6e5382
Shawanda Davis <shawandadavis93@gmail.com>
2018-12-27 17:16:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,18 +7,12 @@ class Book
     @title
   end
   
-  attr_accessors :author
-  attr_writer :author
-  attr_reader :page_count
-  attr_writer :page_count
-  
-  attr_reader :genre 
-  attr_writer :genre
+  attr_accessors :author, :page_count, :genre
+ 
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end  
-  
   
 end
 
